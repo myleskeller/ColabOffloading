b7542fde44668a28127dfafe84d1a5436e69bfe2
Myles Keller <43857093+myleskeller@users.noreply.github.com>
2021-03-24 11:20:16 -0400

    Add files via upload

---
+++ samples.py
@@ -0,0 +1,24 @@
+import bpy
+import sys
+argv = sys.argv
+argv = argv[argv.index("--") + 1:]  # get all args after "--"
+
+
+
+def assign_samples(num_samples):
+    preferences = bpy.context.preferences
+
+    if (bpy.context.scene.render.engine == "CYCLES"):
+        # cycles_preferences = preferences.addons["cycles"].preferences
+        bpy.context.scene.cycles.samples = num_samples
+
+    else if (bpy.context.scene.render.engine == "BLENDER_EEVEE"):
+        bpy.context.scene.eevee.taa_render_samples = num_samples
+        
+    else if (bpy.context.scene.render.engine == "BLENDER_WORKBENCH"):
+        bpy.context.scene.render_aa = num_samples
+
+    return num_samples
+
+
+assign_samples(argv[0]) # assuming that the 1st argument will be samples
